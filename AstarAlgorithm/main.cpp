54e075f0a94a1b556e86563c64cf361f38f5701e
LeeYM <sixk1999@gmail.com>
2021-11-23 04:48:44 +0900

    feat : WhyBitwiseToSlow?

--- AstarAlgorithm/main.cpp
+++ AstarAlgorithm/main.cpp
@@ -2,6 +2,9 @@
 #include <queue>
 #include <vector>
 #include <bitset>
+#include <chrono>
+#include <Windows.h>
+#pragma comment(lib, "winmm.lib")
 
 #define START_X 4
 #define START_Y 5
@@ -13,35 +16,8 @@
 #define INF 1e9
 
 using namespace std;
+using namespace chrono;
 
-int map[MAP_SIZE_X][MAP_SIZE_Y] = {
-	{ 0,0,0,0,0, 0, 0,0,0,0,0 },
-	{ 0,2,2,2,2, 0, 2,2,2,2,0 },
-	{ 0,0,0,0,0, 2, 0,0,0,0,0 },
-	{ 0,0,0,0,0, 2, 0,0,0,0,0 },
-	{ 0,2,2,2,2, 0, 2,2,2,2,0 },
-
-	{ 0,0,0,0,1, 2, 3,0,0,0,0 },
-
-	{ 0,2,2,2,2, 0, 2,2,2,2,0 },
-	{ 0,0,0,0,0, 2, 0,0,0,0,0 },
-	{ 0,0,0,0,0, 2, 0,0,0,0,0 },
-	{ 0,2,2,2,2, 0 ,2,2,2,2,0 },
-	{ 0,0,0,0,0, 0, 0,0,0,0,0 }
-};
-//int map[10][10] = {
-//	{ 1,0,0,0,0, 0,0,0,0,0 },
-//	{ 0,0,0,0,2, 0,0,0,0,0 },
-//	{ 0,0,0,0,2, 0,0,0,0,0 },
-//	{ 0,0,0,0,2, 0,0,0,0,0 },
-//	{ 0,0,0,0,2, 0,0,0,0,3 },
-//	{ 0,0,0,0,2, 0,0,0,0,0 },
-//	{ 0,0,0,0,2, 0,0,0,0,0 },
-//	{ 0,0,0,0,2, 0,0,0,0,0 },
-//	{ 0,0,0,0,2, 0,0,0,0,0 },
-//	{ 0,0,0,0,2, 0,0,0,0,0 }
-//};
-//
 struct Pos
 {
 	int X;
@@ -52,29 +28,28 @@ struct Pos
 	bool operator!=(const Pos& other) const { return !(*this == other); }
 };
 
-void PrintMap()
-{
-	for (int r = 0; r < MAP_SIZE_Y; ++r)
-	{
-		for (int c = 0; c < MAP_SIZE_X; ++c)
-		{
-			if (map[r][c] == 0)
-				cout << " ";
-			else if (map[r][c] == 1)
-				cout << "S";
-			else if (map[r][c] == 2)
-				cout << "B";
-			else if (map[r][c] == 3)
-				cout << "E";
-			else if (map[r][c] == 4)
-				cout << "P";
-		}
-		cout << endl;
-	}
-}
+//void PrintMap()
+//{
+//	for (int r = 0; r < MAP_SIZE_Y; ++r)
+//	{
+//		for (int c = 0; c < MAP_SIZE_X; ++c)
+//		{
+//			if (map[r][c] == 0)
+//				cout << " ";
+//			else if (map[r][c] == 1)
+//				cout << "S";
+//			else if (map[r][c] == 2)
+//				cout << "B";
+//			else if (map[r][c] == 3)
+//				cout << "E";
+//			else if (map[r][c] == 4)
+//				cout << "P";
+//		}
+//		cout << endl;
+//	}
+//}
 
-#define heuristics float
-heuristics Heuristics(Pos a, Pos b)
+float Euclidean(Pos a, Pos b)
 {
 	return sqrtf(pow(abs(a.X - b.X), 2) + pow(abs(a.Y - b.Y), 2));
 }
@@ -84,30 +59,45 @@ int Manhattan(Pos a, Pos b)
 	return abs(a.X - b.X) + abs(a.Y - b.Y);
 }
 
+#define DIA 1.4f
+#define STR 1.0f
+
+int dy[] = { -1, 1,0, 0 ,-1, 1,-1,1 };
+int dx[] = { 0, 0,-1, 1 ,-1,-1, 1,1 };
+float dg[] = { STR,STR,STR,STR,DIA,DIA,DIA,DIA };
+
 void astar(Pos start, Pos end)
 {
-	vector<vector<heuristics>> f;
+	int map[MAP_SIZE_X][MAP_SIZE_Y] = {
+		{ 0,0,0,0,0, 0, 0,0,0,0,0 },
+		{ 0,2,2,2,2, 0, 2,2,2,2,0 },
+		{ 0,0,0,0,0, 2, 0,0,0,0,0 },
+		{ 0,0,0,0,0, 2, 0,0,0,0,0 },
+		{ 0,2,2,2,2, 0, 2,2,2,2,0 },
+
+		{ 0,0,0,0,1, 2, 3,0,0,0,0 },
+
+		{ 0,2,2,2,2, 0, 2,2,2,2,0 },
+		{ 0,0,0,0,0, 2, 0,0,0,0,0 },
+		{ 0,0,0,0,0, 2, 0,0,0,0,0 },
+		{ 0,2,2,2,2, 0 ,2,2,2,2,0 },
+		{ 0,0,0,0,0, 0, 0,0,0,0,0 }
+	};
+
+	vector<vector<float>> f;
 	for (int i = 0; i < MAP_SIZE_Y; ++i)
 		f.emplace_back(MAP_SIZE_X, INF);
 	f[start.Y][start.X] = 0;
 
-	vector<vector<heuristics>> g;
+	vector<vector<float>> g;
 	for (int i = 0; i < MAP_SIZE_Y; ++i)
 		g.emplace_back(MAP_SIZE_X, 0);
-
 
 	Pos path[MAP_SIZE_Y][MAP_SIZE_X] = {};
 	path[start.Y][start.X] = start;
 
-	priority_queue<pair<heuristics, Pos>> pq;
+	priority_queue<pair<float, Pos>> pq;
 	pq.emplace(0, start);
-
-#define DIA 1.4f
-#define STR 1.0f
-
-	int dy[] = { -1, 1,0, 0 ,-1, 1,-1,1 };
-	int dx[] = { 0, 0,-1, 1 ,-1,-1, 1,1 };
-	heuristics dg[] = { STR,STR,STR,STR,DIA,DIA,DIA,DIA };
 
 	unsigned char TLR = 0b11110101;
 	unsigned char BLR = 0b11111010;
@@ -118,35 +108,13 @@ void astar(Pos start, Pos end)
 	unsigned char L = 0b11011111;
 	unsigned char R = 0b11101111;
 	unsigned char db[] = { T,B,L,R,TLR,BLR,LTB,RTB };
-
-	int Count = 0;
+	unsigned char db2[] = { 0b10000000,0b01000000,0b00100000,0b00010000,0b00001000,0b00000100,0b00000010,0b00000001 };
 
 	while (false == pq.empty())
 	{
-		++Count;
-		heuristics w = -pq.top().first;
+		float w = -pq.top().first;
 		Pos pos = pq.top().second;
 		pq.pop();
-
-		cout.setf(ios::fixed);
-		cout.precision(4);
-		cout << w << endl;
-		for (int i = 0; i < MAP_SIZE_Y; ++i)
-		{
-			for (int j = 0; j < MAP_SIZE_X; ++j)
-			{
-				if (g[i][j] > 300)
-					cout << 99.9999 << " ";
-				else if (g[i][j] == 0)
-					cout << "99.9999" << " ";
-				else if (g[i][j] < 10)
-					cout << "0" << g[i][j] << " ";
-				else
-					cout << g[i][j] << " ";
-			}
-			cout << endl;
-		}
-		cout << endl;
 
 		if (pos == end)
 			break;
@@ -169,19 +137,16 @@ void astar(Pos start, Pos end)
 
 		for (int i = 0; i < 8; ++i)
 		{
-			if ((bit & (0b10000000 >> i)) == 0b10000000 >> i)
+			if ((bit & db2[i]) == db2[i])
 			{
 				int nx = dx[i] + pos.X;
 				int ny = dy[i] + pos.Y;
-				heuristics ng = Heuristics({ nx,ny }, pos) + g[pos.Y][pos.X];
-
-				if (ny < 0 || ny >= MAP_SIZE_Y || nx < 0 || nx >= MAP_SIZE_X)
-					continue;
 
 				if (false == (map[ny][nx] == 0 || map[ny][nx] == 3))
 					continue;
 
-				heuristics nf = dg[i] + Heuristics({ nx,ny }, end) + g[pos.Y][pos.X];
+				float ng = Euclidean({ nx,ny }, pos) + g[pos.Y][pos.X];
+				float nf = dg[i] + Euclidean({ nx,ny }, end) + g[pos.Y][pos.X];
 
 				if (f[ny][nx] > nf)
 				{
@@ -195,7 +160,6 @@ void astar(Pos start, Pos end)
 		}
 	}
 
-	cout << Count << endl;
 	Pos curr = end;
 	while (curr != start)
 	{
@@ -205,13 +169,115 @@ void astar(Pos start, Pos end)
 	}
 }
 
+void astar2(Pos start, Pos end)
+{
+	int map2[MAP_SIZE_X][MAP_SIZE_Y] = {
+		{ 0,0,0,0,0, 0, 0,0,0,0,0 },
+		{ 0,2,2,2,2, 0, 2,2,2,2,0 },
+		{ 0,0,0,0,0, 2, 0,0,0,0,0 },
+		{ 0,0,0,0,0, 2, 0,0,0,0,0 },
+		{ 0,2,2,2,2, 0, 2,2,2,2,0 },
+
+		{ 0,0,0,0,1, 2, 3,0,0,0,0 },
+
+		{ 0,2,2,2,2, 0, 2,2,2,2,0 },
+		{ 0,0,0,0,0, 2, 0,0,0,0,0 },
+		{ 0,0,0,0,0, 2, 0,0,0,0,0 },
+		{ 0,2,2,2,2, 0 ,2,2,2,2,0 },
+		{ 0,0,0,0,0, 0, 0,0,0,0,0 }
+	};
+
+	vector<vector<float>> f;
+	for (int i = 0; i < MAP_SIZE_Y; ++i)
+		f.emplace_back(MAP_SIZE_X, INF);
+	f[start.Y][start.X] = 0;
+
+	vector<vector<float>> g;
+	for (int i = 0; i < MAP_SIZE_Y; ++i)
+		g.emplace_back(MAP_SIZE_X, 0);
+
+	Pos path[MAP_SIZE_Y][MAP_SIZE_X] = {};
+	path[start.Y][start.X] = start;
+
+	priority_queue<pair<float, Pos>> pq;
+	pq.emplace(0, start);
+
+	while (false == pq.empty())
+	{
+		float w = -pq.top().first;
+		Pos pos = pq.top().second;
+		pq.pop();
+
+		if (pos == end)
+			break;
+
+		if (f[pos.Y][pos.X] < w)
+			continue;
+
+		for (int i = 0; i < 8; ++i)
+		{
+			int nx = dx[i] + pos.X;
+			int ny = dy[i] + pos.Y;
+
+			if (ny < 0 || ny >= MAP_SIZE_Y || nx < 0 || nx >= MAP_SIZE_X)
+				continue;
+
+			if (false == (map2[ny][nx] == 0 || map2[ny][nx] == 3))
+				continue;
+
+			if ((i == 4 || i == 6) && map2[pos.Y - 1][pos.X] == 2)
+				continue;
+			else if ((i == 5 || i == 7) && map2[pos.Y + 1][pos.X] == 2)
+				continue;
+			else if ((i == 4 || i == 5) && map2[pos.Y][pos.X - 1] == 2)
+				continue;
+			else if ((i == 6 || i == 7) && map2[pos.Y][pos.X + 1] == 2)
+				continue;
+
+			float ng = Euclidean({ nx,ny }, pos) + g[pos.Y][pos.X];
+			float nf = dg[i] + Euclidean({ nx,ny }, end) + g[pos.Y][pos.X];
+
+			if (f[ny][nx] > nf)
+			{
+				f[ny][nx] = nf;
+				g[ny][nx] = ng;
+				path[ny][nx] = pos;
+
+				pq.emplace(-f[ny][nx], Pos{ nx,ny });
+			}
+		}
+	}
+
+	Pos curr = end;
+	while (curr != start)
+	{
+		map2[curr.Y][curr.X] = 4;
+
+		curr = path[curr.Y][curr.X];
+	}
+}
+
 int main()
 {
-	PrintMap();
+	//PrintMap();
 
 	puts("");
 
-	astar({ START_X, START_Y }, { END_X, END_Y });
+	auto start = timeGetTime();
+	for (int i = 0; i < 100000; ++i)
+		astar({ START_X, START_Y }, { END_X, END_Y });
+	auto end = timeGetTime();
 
-	PrintMap();
+	auto start1 = timeGetTime();
+	for (int i = 0; i < 100000; ++i)
+		astar2({ START_X, START_Y }, { END_X, END_Y });
+	auto end1 = timeGetTime();
+
+	unsigned long bit = end - start;
+	unsigned long normal = end1 - start1;
+
+	cout << "   bit : " << bit << endl << endl;
+	cout << "normal : " << normal << endl << endl;
+
+	//PrintMap();
 }
